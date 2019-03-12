c82312816857625681c6c1f13991f14172ad737a
Hashantha Perera <7hashd@gmail.com>
2019-03-12 16:21:01 +0530

    random number generator

--- app/src/main/java/com/example/dicee/MainActivity.java
+++ app/src/main/java/com/example/dicee/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Dicee", "Hello Player !");
+
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+
+                Log.d("Dicee", "The random number is:  " + number);
+            }
+        });
     }
 }
